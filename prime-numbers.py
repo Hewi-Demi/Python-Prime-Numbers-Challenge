e715e85e57a6f80eced626a214c0b9bc3331aecc
Hewan Demessew <hewi@Hewans-MBP.lan>
2023-05-18 18:14:26 +0100

    added my def function to simplify the code

--- prime-numbers.py
+++ prime-numbers.py
@@ -1,13 +1,18 @@
-# Python program to display all the prime numbers within an interval
+prime-numbers.p# Python program to display all the prime numbers within an interval
 
-firstnumber = 1
-lastnumber = 250
 
-print("Prime numbers between", firstnumber, "and", lastnumber, "are:")
+LowerLimit = 1
+UpperLimit = 250
 
-for num in range(firstnumber, lastnumber + 1):
-   # all prime numbers are 1 or greater than 1
-   if num >= 1:
+#Print statement 
+print("Prime numbers between", LowerLimit, "and", UpperLimit, "are:")
+
+#For loop to use a counter to run through all the numbers within the range LowerLimit, UpperLimit. 
+#Need to include +1 because indexing starts at 0 so if you want the UpperLimit variable to 250 you have to use +1.  
+
+for num in range(LowerLimit, UpperLimit + 1):
+   # all prime numbers must be greater than 1
+   if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
                break
